e51d4119ec9d1d516183fb9995bee33ab54724ce
Rob <bassir@michigan.gov>
2024-08-18 10:00:08 -0400

    added initial commit

---
+++ main.tf
@@ -0,0 +1,94 @@
+data "azurerm_virtual_network" "vnet" {
+    name = var.vnet_name
+    resource_group_name = var.resource_group_name
+  
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = data.azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = data.azurerm_virtual_network.vnet.location
+  resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name
+
+  security_rule {
+    name                       = "Allow-Internet-Outbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
+
+  security_rule {
+    name                       = "Deny-All-Inbound"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = data.azurerm_virtual_network.vnet.location
+  resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  location            = data.azurerm_virtual_network.vnet.location
+  resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name
+  size                = "Standard_B1ls" # Free tier
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name  = var.vm_name
+  disable_password_authentication = true
+  provision_vm_agent              = true
+  allow_extension_operations      = true
+
+  tags = {
+    environment = "Terraform"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+  default     = "myVnet"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+  default     = "mySubnet"
+}
+
+variable "nsg_name" {
+  description = "The name of the Network Security Group"
+  type        = string
+  default     = "myNSG"
+}
+
+variable "nic_name" {
+  description = "The name of the Network Interface"
+  type        = string
+  default     = "myNic"
+}
+
+variable "vm_name" {
+  description = "The name of the virtual machine"
+  type        = string
+  default     = "myVM"
+}
+
+variable "admin_username" {
+  description = "The admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "The path to the SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
