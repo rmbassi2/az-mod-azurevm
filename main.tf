fd9e245ca6497bd932712143c949148c5484f573
rmbassi2 <131687456+rmbassi2@users.noreply.github.com>
2025-03-07 13:28:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.115.0" // specify the required version of the AzureRM provider
+      version = "> 3.0" // specify the required version of the AzureRM provider
     }
   }
 }
